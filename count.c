c55aa92019cdac2779bfd36ba17ece5724375174
gokulavani <dhilipkumarvv@gmail.com>
2017-06-07 10:22:54 +0530

    Add files via upload

---
+++ count.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int n,i,count,sum=0;
+printf("Enter the value of n:");
+scanf("%d",&n):
+for(i=1;i<=n;i++)
+{
+sum=sum+i;
+count=count+1;
+}
+printf("The count is:%count",count);
+} 
